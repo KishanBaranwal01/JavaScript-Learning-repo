d9002920cb6ea123bd11dfe56dedf8793fbf07dc
Kishan Baranwal <158753644+KishanBaranwal01@users.noreply.github.com>
2025-08-21 13:42:06 +0000

    variable done

--- 01_Basics/Test.js
+++ 01_Basics/Test.js
@@ -1,1 +1,16 @@
-console.log("Har Har Mahadev");
+console.log("Har Har Mahadev"); 
+
+const Ac_id = 121 ;
+let AC_mail = "k@gmail.com" ;
+var Ac_pas  = "12345";
+Ac_city = "jaipur"
+
+// Ac_id = 12 ;
+console.log(Ac_id);
+
+console.table([Ac_id,Ac_pas,AC_mail,Ac_city]);
+
+/* This is multi line comment system 
+*/
+// This is single line comment
+
