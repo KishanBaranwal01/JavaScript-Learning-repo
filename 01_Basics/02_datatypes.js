43433abe58dce540b4a12fd4d1911a3d54ea15ce
Kishan Baranwal <158753644+KishanBaranwal01@users.noreply.github.com>
2025-08-23 03:07:16 +0000

    commite datatype

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,26 @@
+//  Basically in js there are 2 typesof data types which are 1 - Primitive Data Type and 2 - Non-Primitive Datatype
+/* 
+        =>  Primitive Datatypes are aslo categorised into 5 types which are as follows -
+               Number , String , Boolean , null and undefined .
+         =>   primitive data types has fix value they can't be change .immutable 
+         =>   It is used to store single value like let x = 10 ;
+             
+        
+        => Non-Primitive Datatypes are also categorised in to types which are as -
+              Array , function , obejct , date and reglar expression     
+            => This is the mutable in nature and stores the refrences of the value . 
+            =>  It is used to store the multiple value like array and object.   
+ */
+
+            console.log("DataTypes Test");
+            
+            // We can use Type of Operator to find the type of the any kind of datatype of values.
+
+            console.log(typeof(10));
+             console.log(typeof(10.10));
+              console.log(typeof("10"));
+               console.log(typeof(true));
+                console.log(typeof(null));
+                 console.log(typeof(undefined));
+
+            
